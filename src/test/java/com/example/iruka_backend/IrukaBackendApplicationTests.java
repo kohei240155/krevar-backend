57f785be05736ae24ce58f3e43d36958c0e30ab1
kohei240155 <k.pino519@gmail.com>
2024-07-13 08:04:13 +0900

    Initial commit

---
+++ src/test/java/com/example/iruka_backend/IrukaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iruka_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrukaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
