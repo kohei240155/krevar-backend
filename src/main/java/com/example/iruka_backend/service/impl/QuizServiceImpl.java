cc2db406700ac67caba60e501ef911cd2222d53c
kohei240155 <k.pino519@gmail.com>
2024-08-12 21:11:28 +0900

    不要なコメントを削除

--- src/main/java/com/example/iruka_backend/service/impl/QuizServiceImpl.java
+++ src/main/java/com/example/iruka_backend/service/impl/QuizServiceImpl.java
@@ -15,14 +15,10 @@ public class QuizServiceImpl implements QuizService {
 
 	@Autowired
 	private QuizRepository quizRepository;
-	
+
 	@Override
 	public List<Word> getQuestionsByDeckId(Long deckId) {
-//		LocalDateTime now = LocalDateTime.now();
-//		TODO: 動作確認用
 		LocalDateTime now = LocalDateTime.parse("2024-07-18T12:00:00");
-		System.out.println("★：" + now.toString());
-		System.out.println("★：" + deckId);
 		return quizRepository.findTodaysWords(now, deckId);
 	}
 
