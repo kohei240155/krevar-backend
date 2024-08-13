234aaac62659dab3b7768e0e64094787f1dc1004
kohei240155 <k.pino519@gmail.com>
2024-08-13 18:27:46 +0900

    BaseEntityを新規作成

---
+++ src/main/java/com/example/iruka_backend/common/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.example.iruka_backend.common.entity;
+
+import java.sql.Timestamp;
+
+public class BaseEntity {
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
+
+    // Getters and Setters
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Timestamp getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Timestamp deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+}
