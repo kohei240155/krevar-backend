87e4cf51735bee42fc7428953960c5f994fb3fed
kohei240155 <k.pino519@gmail.com>
2024-08-18 19:26:04 +0900

    DDLを修正

--- src/main/resources/db/reset.sql
+++ src/main/resources/db/reset.sql
@@ -1,10 +1,10 @@
 SET SQL_SAFE_UPDATES = 0;
 UPDATE `iruka_db`.`words` SET `is_normal_mode_correct` = '0';
 UPDATE `iruka_db`.`words` SET `is_extra_mode_correct` = '0';
-UPDATE `iruka_db`.`words` SET `next_practice_date` = '2024-08-17 00:00:00';
+-- UPDATE `iruka_db`.`words` SET `next_practice_date` = '2024-08-17 00:00:00';
 UPDATE `iruka_db`.`words` SET `correct_count` = '0', `incorrect_count` = '0';
 UPDATE `iruka_db`.`words` SET `review_interval_id` = '1';
 SET SQL_SAFE_UPDATES = 1;
 
-
 select * from words;
+select * from words where deck_id = 1;
